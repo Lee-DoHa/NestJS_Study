065cd28d3c72b0814a5271fc478d80950265e8f2
Lee-DoHa <johnny550@naver.com>
2023-04-02 16:01:38 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { ApiController } from './api/api.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
+  controllers: [ApiController, AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
